6d15ee585bd536f367ac4d51c9cb61d800cd9aa3
Sam Lerner <lerner98@gmail.com>
2020-10-18 14:45:58 -0400

    load dylib changes

--- iOS/build.sh
+++ iOS/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cp client/Spotify spmod
-cp ~/Library/Developer/Xcode/DerivedData/SkipTracer-gwcgrcitumrdjncbxhuwxnazwvkr/Build/Products/Debug-iphoneos/SkipTracer.framework/SkipTracer Payload/Spotify.app/Frameworks/libskip.dylib 
+cp ~/Library/Developer/Xcode/DerivedData/SkipTracer-ecfucjjhxjcyzzgyizjsbjjdblju/Build/Products/Debug-iphoneos/SkipTracer.framework/SkipTracer Payload/Spotify.app/Frameworks/libskip.dylib 
 python ../common/load_dylib.py --binary spmod \
 	--dylib @rpath/libskip.dylib
 cp spmod Payload/Spotify.app/Spotify
