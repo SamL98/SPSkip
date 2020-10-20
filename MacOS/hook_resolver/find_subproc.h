75330e79841df86a359d21ae2c263ddd40ca6297
Sam Lerner <lerner98@gmail.com>
2020-10-19 20:08:29 -0400

    removed hook_resolver stale directory

--- MacOS/hook_resolver/find_subproc.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-uint64_t find_subproc(FILE *fp, 
-					  uint64_t imp_ptr,
-					  int64_t text_addend,
-					  int32_t **reloc_addr,
-					  int64_t *reloc_pc);
--- MacOS/hook_resolver/spresolver.h
+++
@@ -1,3 +0,0 @@
-void (*resolve_mediaKey_subproc_addr(uint64_t imp_ptr,
-									 int32_t **reloc_addr, 
-									 int64_t *reloc_pc))(void *, int32_t);
